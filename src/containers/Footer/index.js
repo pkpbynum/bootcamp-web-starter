29ef958ca43e3a89a84de66223f63e96f60e8d62
LucyJ <lucyjiao1215@gmail.com>
2020-01-22 14:55:16 -0500

    corrected Alvin

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -3,7 +3,7 @@ import { FootContainer, FootText } from './styles'
 
 const Footer = () => (
   <FootContainer>
-    <FootText>Created by Marco Antonio, Lucy Jiao, Maegan Jong, Amy Zhou, and Alivin Zou</FootText>
+    <FootText>Created by Marco Antonio, Lucy Jiao, Maegan Jong, Amy Zhou, and Alvin Zou</FootText>
   </FootContainer>
 )
 
